8b19ac15fc5aabf223b6ec52765a60e94cce7fc3
FUTRADERX <asedrew117@gmail.com>
2025-04-17 20:05:52 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container">
-      <h1 className="logo">FUTRADERX</h1>
-      <p className="tagline">Get paid to trade. No cap, no fluff.</p>
-      <button className="start-btn">Start Challenge</button>
-    </div>
-  )
-}
